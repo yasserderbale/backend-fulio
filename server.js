96e695d72df3a5b0989071e3f6594267a0bb4500
yasserderbale <drbalyasser@gmaile.com>
2024-11-16 17:40:36 +0100

    update

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.post("/contact", async (req, res) => {
     console.log(req.body)
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS
