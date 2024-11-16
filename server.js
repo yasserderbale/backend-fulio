0df7b33bc54ac4537cb287564cddf750166581fa
yasserderbale <drbalyasser@gmaile.com>
2024-11-16 17:08:18 +0100

    update

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const nodemailer = require('nodemailer');
+require('dotenv').config();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
